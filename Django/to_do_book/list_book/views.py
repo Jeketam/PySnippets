fbc2b2a7aeaccbfd745f6d48c0c712b71f1da7e8
Jeketam <jeketam@gmail.com>
2018-08-01 23:26:50 -0400

    Added views and urls

--- Django/to_do_book/list_book/views.py
+++ Django/to_do_book/list_book/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+
+def task_list(request):
+    tasks = Task.published.all()
+    return render(request, 'blog/task/list.html', {'tasks': tasks})
